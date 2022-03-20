78ead422090c73382767d4e3e03e34dc4e1ff8ac
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-20 18:37:01 +0300

    UI

--- components/Cssbattle.js
+++ components/Cssbattle.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Cssbattle() {
   return (
-    <div>Cssbattle</div>
-  )
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-orange-400 w-60 h-60 rounded-full">
+        <div className="flex justify-between m-12">
+          <div className="bg-black h-14 w-10 rounded-full z-40"></div>
+          <div className="bg-black h-14 w-10 rounded-full z-40"></div>
+        </div>
+        {/*  */}
+        <div className="bg-black h-36 w-36 rounded-full absolute top-[42%] left-[45%] "></div>
+        <div className="bg-orange-400 h-36 w-36 mx-auto rounded-full absolute top-[38%] left-[45%]   "></div>
+      </div>
+    </div>
+  );
 }
 
-export default Cssbattle
+export default Cssbattle;
