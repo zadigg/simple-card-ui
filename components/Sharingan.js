edd314c320eac6ddee41f2c9a0528c1f0f5765d4
zadigg <62830716+zadigg@users.noreply.github.com>
2022-03-20 19:36:23 +0300

    UI

--- components/Sharingan.js
+++ components/Sharingan.js
@@ -5,7 +5,9 @@ function Sharingan() {
     <div className="flex h-screen items-center justify-center bg-black  ">
       <div className="w-60 h-60 bg-[#dd6b4d] rounded-full shadow-nprogress ">
         <div></div>
-        <div>hi</div>
+        <div>
+          hi
+        </div>
       </div>
     </div>
   );
